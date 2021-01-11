b2c33830bbb766d3e0ee1400d1fa0ff679f59e6e
Hristo Kalinov <76777296+hristo-1979@users.noreply.github.com>
2021-01-11 16:30:00 +0200

    new version

--- test-1.py
+++ test-1.py
@@ -6,8 +6,10 @@ window.geometry("450x350")
 
 label1 = tk.Label(text="Hello there!")
 button1 = tk.Button(text="Click Me!")
+label2 = tk.Label(text="I am number 2")
 
 label1.pack()
 button1.pack()
+label2.pack()
 
 window.mainloop()
