49572b05bef05a72397ed23cd5b1e09f487da0bf
Hristo Kalinov <76777296+hristo-1979@users.noreply.github.com>
2021-01-12 15:47:08 +0200

    new version

--- test-function-from-file.py
+++ test-function-from-file.py
@@ -4,16 +4,25 @@ from onclick import onclick
 def onclick_local():
     label1.configure(text=onclick(label1.cget("text")))
 
+def btn2_click():
+    label1.configure(text="result")
+
 window = tk.Tk()
 window.geometry("450x350")
 window.title("Test App")
 
-label1 = tk.Label(text="result")
-label2 = tk.Label(text="Click the button")
-button1 = tk.Button(text="Click Me!", command=onclick_local)
+label1 = tk.Label(text="result", font="Helvetica 12 bold", bg="gray"
+                  , fg="white", height=5, width=20)
+label2 = tk.Label(text="Click the button", font="Helvetica 12 bold"
+                  , bg="magenta", fg="white", height=1, width=20)
+button1 = tk.Button(text="Click Me!", font="Helvetica 12 bold", bg="red"
+                    , fg="white", height=5, width=20, command=onclick_local)
+button2 = tk.Button(text="Reset", font="Helvetica 12 bold", bg="yellow"
+                    , fg="white", height=5, width=20, command=btn2_click)
 
 label2.pack()
 button1.pack()
+button2.pack()
 label1.pack()
 
 window.mainloop()
