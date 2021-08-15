3315626833bebdacbae4fb543d54c8b523c9c675
Hristo Kalinov <hristo.kalinov@gmail.com>
2021-08-15 17:49:07 +0300

    ok

--- test2.py
+++ test2.py
@@ -4,8 +4,8 @@ window = tk.Tk()
 window.geometry('600x480')
 window.title('test2 program')
 
-label1 = tk.Label(text='Hello World!')
-button1 = tk.Button(text='Click Me!')
+label1 = tk.Label(text='Hello World!', bg='red', fg='white')
+button1 = tk.Button(text='Click Me!', bg='yellow', fg='blue')
 
 label1.pack()
 button1.pack()
