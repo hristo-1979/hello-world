4ab6c48749e76d79ff61ad1febda01b2e196bb86
Hristo Kalinov <hristo.kalinov@gmail.com>
2021-09-01 18:07:45 +0300

    two windows after click

--- test2.py
+++ test2.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 
+def opnwindow2():
+    window2 = tk.Tk()
+    window2.geometry('200x100')
+    window2.title('window2')
+
 window1 = tk.Tk()
 window1.geometry('640x480')
 window1.title('test2 program')
 
-window2 = tk.Tk()
-window2.geometry('200x100')
-window2.title('window2')
-
 label1 = tk.Label(text='Hello World!', bg='red', fg='white')
-button1 = tk.Button(text='Click Me!', bg='yellow', fg='blue')
+button1 = tk.Button(text='Click Me!', bg='yellow', fg='blue', command=opnwindow2)
 
 label1.pack()
 button1.pack()
 
 window1.mainloop()
-window2.mainloop()
