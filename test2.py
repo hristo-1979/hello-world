ed4603e295c51f4881383e8c2a266a88e6d115a7
Hristo Kalinov <hristo.kalinov@gmail.com>
2021-09-01 12:28:35 +0300

    two windows at startup

--- test2.py
+++ test2.py
@@ -1,8 +1,12 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.geometry('640x480')
-window.title('test2 program')
+window1 = tk.Tk()
+window1.geometry('640x480')
+window1.title('test2 program')
+
+window2 = tk.Tk()
+window2.geometry('200x100')
+window2.title('window2')
 
 label1 = tk.Label(text='Hello World!', bg='red', fg='white')
 button1 = tk.Button(text='Click Me!', bg='yellow', fg='blue')
@@ -10,4 +14,5 @@ button1 = tk.Button(text='Click Me!', bg='yellow', fg='blue')
 label1.pack()
 button1.pack()
 
-window.mainloop()
+window1.mainloop()
+window2.mainloop()
